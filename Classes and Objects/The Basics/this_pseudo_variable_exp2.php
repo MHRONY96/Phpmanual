c3df1987979ec100e0292ddf5cf1f9903e55f39b
unknown <eng.hanif.roni63f@gmail.com>
2022-05-31 14:23:47 +0600

    Add Classes and Object

---
+++ Classes and Objects/The Basics/this_pseudo_variable_exp2.php
@@ -0,0 +1,24 @@
+<?php
+class A{
+ function foo(){
+  if(isset($this)){
+   echo '$this is defined (';
+   echo get_class($this);
+   echo ")\n";
+  }else{
+   echo "\$this is not defined.\n";
+  }
+ }
+}
+
+class B{
+ function Bar(){
+  A::foo();
+ }
+}
+$a= new A;
+$a->foo();
+// A::foo();
+// $b=new B;
+// $b->Bar();
+// B::Bar();
